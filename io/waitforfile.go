85e76ededd9760b5061ada78b659e1dce927fddb
Niels Gregers Johansen <niels@jumpto365.com>
2023-06-09 09:05:57 +0200

    added for loop

--- io/waitforfile.go
+++ io/waitforfile.go
@@ -8,18 +8,19 @@ import (
 )
 
 func Waitfor(filePath string) {
+	for {
+		_, err := os.Open(filePath)
 
-	_, err := os.Open(filePath)
+		if err == nil {
+			log.Println("Found", filePath)
+			log.Println("Waiting 2 secs")
+			time.Sleep(2 * time.Second)
+			log.Println("Continuing")
+			return
+		}
 
-	if err == nil {
-		log.Println("Found", filePath)
-		log.Println("Waiting 2 secs")
-		time.Sleep(2 * time.Second)
-		log.Println("Continuing")
-		return
+		time.Sleep(5 * time.Second)
+		log.Println("Waiting for", filePath)
 	}
-
-	time.Sleep(5 * time.Second)
-	log.Println("Waiting for", filePath)
 
 }
