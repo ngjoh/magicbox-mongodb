3c3a5aaa8db1d61cc554bcb483f326e2ae08e132
Niels Gregers Johansen <niels@jumpto365.com>
2024-02-05 13:53:44 +0100

    initial steps

---
+++ 40 backup/10 database discover.ps1
@@ -0,0 +1,32 @@
+<#---
+title: Database Discovery
+description: Discover databases in the cluster
+connection: sharepoint
+output: databaseservices.json
+---#>
+#$env:WORKDIR="/Users/nielsgregersjohansen/kitchens/kubernetes-management/.koksmat/workdir"
+
+$services = kubectl get services -n percona -o json | ConvertFrom-Json
+
+$result = @()
+$databaseservices = @()
+foreach ($item in $services.items) {
+    $match = [string]$item.metadata.name.EndsWith("-mongos")
+    if ($match -eq "True") {
+        $result += $item
+        $databaseservices += @{
+            name = $item.metadata.name
+            namespace = $item.metadata.namespace
+           
+        }
+    }
+}
+
+
+
+
+
+
+
+ConvertTo-Json -Depth 10 -InputObject $databaseservices
+| Out-File "$env:WORKDIR/databaseservices.json"
--- readme.md
+++ readme.md
@@ -4,3 +4,19 @@ description: Describe the main purpose of this kitchen
 ---
 
 # kubernetes-management
+
+
+```mermaid
+
+sequenceDiagram
+
+    Operator->>+Database: Database, can you hear me?
+    Database-->>-Operator: Hi Operator, I can hear you!
+    Operator->>+Database: Make a backup
+    Database-->>-Operator: I'm done
+    Operator->>+Database: Upload it to Azure Blob
+    Database->>+Azure: Store this
+    Azure-->>-Database: Done
+    Database-->-Operator: Done
+    
+``````
